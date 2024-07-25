e0abe00ba32fe4f0b468da308a93b13c6c33e581
MaximumG9 <maximeguy9@gmail.com>
2024-07-24 18:58:21 -0700

    Reduced shadow tnt damage

---
+++ src/main/kotlin/dev/osmii/shadow/events/custom/abilities/HandleAbilityTNTExplosion.kt
@@ -0,0 +1,29 @@
+package dev.osmii.shadow.events.custom.abilities
+
+import dev.osmii.shadow.Shadow
+import dev.osmii.shadow.enums.PlayableFaction
+import dev.osmii.shadow.enums.PlayableRole
+import org.bukkit.damage.DamageType
+import org.bukkit.entity.Player
+import org.bukkit.event.EventHandler
+import org.bukkit.event.Listener
+import org.bukkit.event.entity.EntityDamageEvent
+
+class HandleAbilityTNTExplosion(val shadow: Shadow) : Listener {
+    @EventHandler
+    fun handleExplosion(e : EntityDamageEvent) {
+        if (e.damageSource.damageType == DamageType.EXPLOSION) {
+            val entity = e.damageSource.directEntity?.uniqueId
+            if (shadow.spawnedTNTs.stream().anyMatch { entity == it }) {
+                if (e.entity is Player) {
+                    val player = e.entity as Player
+                    if(shadow.gameState.currentRoles.getOrDefault(player.uniqueId,PlayableRole.SPECTATOR.roleFaction) == PlayableFaction.SHADOW) {
+                        e.isCancelled = true
+                        player.damage(0.1)
+                    }
+                }
+                shadow.spawnedTNTs.remove(entity)
+            }
+        }
+    }
+}
