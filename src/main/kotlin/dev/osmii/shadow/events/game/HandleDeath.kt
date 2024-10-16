a97439135643f484886f88c9ba3b6b86d84ff4bd
MaximumG9 <maximeguy9@gmail.com>
2024-10-15 23:26:06 -0700

    Made death messages nicer.

--- src/main/kotlin/dev/osmii/shadow/events/game/HandleDeath.kt
+++ src/main/kotlin/dev/osmii/shadow/events/game/HandleDeath.kt
@@ -1,5 +1,6 @@
 package dev.osmii.shadow.events.game
 
+import com.mojang.logging.LogUtils
 import dev.osmii.shadow.Shadow
 import dev.osmii.shadow.enums.GamePhase
 import dev.osmii.shadow.enums.PlayableFaction
@@ -10,7 +11,6 @@ import net.kyori.adventure.text.format.NamedTextColor
 import net.kyori.adventure.text.minimessage.MiniMessage
 import org.bukkit.Bukkit
 import org.bukkit.GameMode
-import org.bukkit.Sound
 import org.bukkit.event.EventHandler
 import org.bukkit.event.EventPriority
 import org.bukkit.event.Listener
@@ -18,7 +18,7 @@ import org.bukkit.event.entity.PlayerDeathEvent
 import org.bukkit.event.player.PlayerRespawnEvent
 
 class HandleDeath(private val shadow: Shadow) : Listener {
-    var checking = false
+    private var checking = false
 
     // Handles player deaths and sheriff misfires
     @EventHandler(priority = EventPriority.LOW)
@@ -30,28 +30,27 @@ class HandleDeath(private val shadow: Shadow) : Listener {
 
         // Choose color and role
         val p = e.entity
-        val color = shadow.gameState.currentRoles[p.uniqueId]?.roleColor
-        val message = shadow.gameState.currentRoles[p.uniqueId]?.roleName
-        for (player in shadow.server.onlinePlayers) {
-            player.playSound(player.location, Sound.ENTITY_LIGHTNING_BOLT_THUNDER, 1f, 1f)
-            player.sendMessage(
-                Component.text(p.name + " died. They were a ")
-                    .color(color)
-                    .append(Component.text(message.toString()).color(color))
-                    .append(Component.text("."))
-            )
-            if (shadow.gameState.currentRoles[p.uniqueId]?.roleFaction == PlayableFaction.SHADOW) {
-                player.sendMessage(
-                    Component.text("There are ")
-                        .color(NamedTextColor.RED)
-                        .append(
-                            Component.text("${shadow.gameState.currentRoles.filter { (_, role) -> role.roleFaction == PlayableFaction.SHADOW }.size - 1} ")
-                                .color(NamedTextColor.GOLD)
-                        )
-                        .append(Component.text("shadows remaining.").color(NamedTextColor.RED))
-                )
-            }
-        }
+        val playerRole = shadow.gameState.currentRoles[p.uniqueId]
+        val color = playerRole?.roleColor
+        val message = playerRole?.roleName
+
+        e.deathMessage(
+            Component.text(p.name + " died. They were a ")
+                .color(color)
+                .append(Component.text(message.toString()).color(color))
+                .append(Component.text("."))
+                .apply {
+                    if (playerRole?.roleFaction == PlayableFaction.SHADOW) {
+                        this.append(Component.text("There are "))
+                            .color(NamedTextColor.RED)
+                            .append(
+                                Component.text("${shadow.gameState.currentRoles.filter { (_, role) -> role.roleFaction == PlayableFaction.SHADOW }.size - 1} ")
+                                    .color(NamedTextColor.GOLD)
+                            )
+                            .append(Component.text("shadows remaining.").color(NamedTextColor.RED))
+                    }
+                }
+        )
 
         shadow.gameState.currentRoles[p.uniqueId] = PlayableRole.SPECTATOR
 
