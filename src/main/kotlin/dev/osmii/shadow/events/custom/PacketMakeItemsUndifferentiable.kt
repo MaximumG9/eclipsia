6b5baf301507e18c26a049d691d7a212cf1db139
MaximumG9 <maximeguy9@gmail.com>
2024-11-29 20:45:16 -0800

    Make item not cycle to not make trident differentiable

--- src/main/kotlin/dev/osmii/shadow/events/custom/PacketMakeItemsUndifferentiable.kt
+++ src/main/kotlin/dev/osmii/shadow/events/custom/PacketMakeItemsUndifferentiable.kt
@@ -21,17 +21,17 @@ class PacketMakeItemsUndifferentiable(val shadow: Shadow) : PacketAdapter(
         val inventory = e.packet.itemListModifier.read(0)
         for(i in 0..<inventory.size) {
             if(ItemUtil.customIdIs(inventory[i], CID.FAKE_SPECIAL_ITEM) || ItemUtil.customIdIs(inventory[i], CID.TRIDENT) || ItemUtil.customIdIs(inventory[i], CID.INVENTORY_SHERIFF_BOW)) {
-                inventory[i] = ItemStack(fakeItems.random())
+                inventory[i] = fakeItem
             }
         }
         val heldSlot = e.packet.itemModifier.read(0)
         if(ItemUtil.customIdIs(heldSlot, CID.FAKE_SPECIAL_ITEM) || ItemUtil.customIdIs(heldSlot, CID.TRIDENT) || ItemUtil.customIdIs(heldSlot, CID.INVENTORY_SHERIFF_BOW)) {
-            e.packet.itemModifier.write(0,ItemStack(fakeItems.random()))
+            e.packet.itemModifier.write(0,fakeItem)
         }
         e.packet.itemListModifier.write(0,inventory)
     }
 
     companion object {
-        val fakeItems: Array<Material> = arrayOf(Material.MUSIC_DISC_5,Material.MUSIC_DISC_11,Material.ARROW,Material.BLUE_DYE,Material.RED_DYE,Material.WHITE_DYE,Material.ARROW,Material.BLAZE_ROD,Material.BLAZE_POWDER,Material.CHARCOAL,Material.COAL,Material.RAW_IRON,Material.IRON_INGOT,Material.AMETHYST_SHARD,Material.NETHERITE_SCRAP,Material.CLOCK,Material.CLAY_BALL,Material.DIAMOND,Material.FLINT,Material.EMERALD,Material.COMPASS,Material.BOOK,Material.BONE)
+        val fakeItem = ItemStack(Material.BLAZE_ROD)
     }
 }
--- src/main/kotlin/dev/osmii/shadow/events/custom/PacketMakeItemsUndifferentiableSingle.kt
+++ src/main/kotlin/dev/osmii/shadow/events/custom/PacketMakeItemsUndifferentiableSingle.kt
@@ -7,10 +7,8 @@ import com.comphenix.protocol.events.PacketEvent
 import dev.osmii.shadow.Shadow
 import dev.osmii.shadow.enums.CID
 import dev.osmii.shadow.enums.RoleModifier
-import dev.osmii.shadow.events.custom.PacketMakeItemsUndifferentiable.Companion.fakeItems
+import dev.osmii.shadow.events.custom.PacketMakeItemsUndifferentiable.Companion.fakeItem
 import dev.osmii.shadow.util.ItemUtil
-import org.bukkit.Material
-import org.bukkit.inventory.ItemStack
 
 class PacketMakeItemsUndifferentiableSingle(val shadow: Shadow) : PacketAdapter(
     shadow,
@@ -22,7 +20,7 @@ class PacketMakeItemsUndifferentiableSingle(val shadow: Shadow) : PacketAdapter(
 
         val modifiedSlot = e.packet.itemModifier.read(0)
         if(ItemUtil.customIdIs(modifiedSlot, CID.FAKE_SPECIAL_ITEM) || ItemUtil.customIdIs(modifiedSlot, CID.TRIDENT) || ItemUtil.customIdIs(modifiedSlot, CID.INVENTORY_SHERIFF_BOW)) {
-            e.packet.itemModifier.write(0,ItemStack(fakeItems.random()))
+            e.packet.itemModifier.write(0,fakeItem)
         }
     }
 }
